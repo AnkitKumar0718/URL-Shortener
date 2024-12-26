96e54e15ade72f8d9910ea9b2308a1bde7bc9933
Ankit Kumar <ankitchauhandhoni@gmail.com>
2024-12-26 12:36:22 +0530

    Your commit message here

--- Config/passportConfig.js
+++ Config/passportConfig.js
@@ -4,8 +4,8 @@ const User = require('../Models/user');
 
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: 'http://localhost:5000/api/auth/google/callback',
+    clientSecret: pr5000ocess.env.CLIENT_SECRET,
+    callbackURL: 'https://url-shortener-tc79.onrender.com/api/auth/google/callback',
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         let user = await User.findOne({ googleId: profile.id });
